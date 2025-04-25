223570cdba563b42aa6c8a2f3d62fa4d6ca83658
YvetteBu <yb2496@nyu.edu>
2025-04-25 15:43:42 -0400

    remove devcerts and update for deployment

--- devCerts.js
+++
@@ -1,8 +0,0 @@
-import devCerts from "office-addin-dev-certs";
-
-export default {
-  getHttpsServerOptions: async () => {
-    const options = await devCerts.getHttpsServerOptions();
-    return options;
-  }
-}; 
